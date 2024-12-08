2fd68255c716eec8b15bf07f54d3c77926683a40
seb hall <sebhalldesigns@gmail.com>
2024-12-08 17:32:21 +0000

    modify wasm

--- src/wasm/mod.rs
+++ src/wasm/mod.rs
@@ -1,8 +1,33 @@
-pub struct MacosContext 
+
+use crate::{Context, AppDelegate, geometry::*};
+
+pub struct PlatformContext 
 {
-    pub app_delegate: Retained<AppDelegate>,
-    pub main_thread_marker: MainThreadMarker,
-    pub pool: Retained<NSAutoreleasePool>,
-    pub app: Retained<NSApplication>,
-    pub metal_device: Retained<ProtocolObject<dyn MTLDevice>>
+    
+}
+
+pub struct PlatformWindow
+{
+    
+}
+
+pub fn create_context(title: String) -> PlatformContext 
+{
+
+   
+    return PlatformContext 
+    {
+       
+    }
+}
+
+pub fn run(context: Context, app_delegate: Box<dyn AppDelegate>, window_size_request: Size) 
+{
+   
+
+    unsafe 
+    {
+        
+    }
+    
 }
